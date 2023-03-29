d8583482fa618dc5ad311b381ba554ede6d8edd1
Hlova Oksana <glovaoksanka@gmail.com>
2023-03-29 15:55:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+property_transfer_xml = """
+<con:name>AuthTocken</con:name><con:sourceType>Response</con:sourceType><con:sourceStep>login</con:sourceStep><con:sourcePath>declare namespace ns1='urn:Magento';
+//ns1:loginResponse/loginReturn[1]</con:sourcePath><con:targetType>Request</con:targetType><con:targetStep>multiCall</con:targetStep><con:targetPath>declare namespace urn='urn:Magento';
+//urn:multiCall/sessionId['?']</con:targetPath>
+"""
+
+splited_data = property_transfer_xml.split("con:targetType>")
+res = splited_data[1].strip("/")
+print(res[:-1])
